c6c66c4afab89d74bfea6922887ca783959a2fd3
FBartos <f.bartos96@gmail.com>
2023-03-30 15:05:50 +0200

    version bump

--- R/utilities.R
+++ R/utilities.R
@@ -78,6 +78,8 @@ assign("max_cores",       parallel::detectCores(logical = TRUE) - 1,  envir = Ro
     paste0(RoBTT.version, collapse = "."),
     "1.0.0" = c("0.2.12", "999.999.999"),
     "1.0.1" = c("0.2.12", "999.999.999"),
+    "1.0.2" = c("0.2.12", "999.999.999"),
+    "1.0.3" = c("0.2.12", "999.999.999"),
     stop("New RoBTT version needs to be defined in '.check_BayesTools' function!")
   )
   
